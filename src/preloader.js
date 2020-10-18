d294c17a3627b51761dcfdcac5c305e3b1a888ff
Tyler Spaulding <TylerSpaulding95@gmail.com>
2020-10-18 01:32:14 -0400

    Refactoring preloader to be pure for reuse later, added logo and start button

--- src/preloader.js
+++ src/preloader.js
@@ -1,22 +1,13 @@
 import Phaser from 'phaser'; 
-import MenuScene from './scenes/MenuScene';
 
 class Preloader extends Phaser.Scene {
     constructor ()
     {
         super('Preloader');
     }
-
-    preload ()
+    preload (context, type, name, path)
     {
-        this.facebook.once('startgame', this.startGame, this);
-        this.facebook.showLoadProgress(this);
-        this.load.image('logo', 'src/assets/logo.png');
-    }
-
-    startGame ()
-    {
-        this.scene.start(MenuScene);
+        context.load[type](name, path);
     }
 }
 
--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -1,21 +1,31 @@
 import 'phaser';
+import Preloader from '../preloader';
 
 class MenuScene extends Phaser.Scene {
     constructor() {
         super({
             key: 'Game',
-            active: false
+            active: false,
         })
     }
 
     preload() {
-        this.load.image('logo', './src/assets/pitbull.png');
+       console.log(this);
+       const preloader = new Preloader();
+       preloader.preload(this, 'image', 'logo', '../src/assets/pitbull.png');
+       preloader.preload(this, 'image', 'start_button', '../src/assets/ui/start_button.png');
     }
 
     create() {
+        //Variables 
         const width = this.game.config.width;
         const height = this.game.config.height;
+        //Change game background color
+        const color = new Phaser.Display.Color.HexStringToColor('#95bacc');
+        this.add.rectangle(width / 2, height / 2, width, height, color.color);
+        //Assets 
         const logo = this.add.image(width / 2, height / 2, 'logo');
+        const start_button = this.add.image(width - 100, height - 100, 'start_button');
         
         this.tweens.add({
             targets: logo,
